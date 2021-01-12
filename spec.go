457944c57380185a7ba84db9dd8803477a6074cb
Zhao KK <kikakkz@hotmail.com>
2021-01-12 23:36:01 +0800

    Support dmi output

--- spec.go
+++ spec.go
@@ -3,15 +3,23 @@ package machspec
 import (
 	"errors"
 	dmidecode "github.com/dselans/dmidecode"
+	"os/user"
 )
 
 type MachineSpec struct {
 }
 
 func ReadMachineSpec() (*MachineSpec, error) {
+	usr, err := user.Current()
+	if nil != err {
+		return nil, err
+	}
+	if "root" != usr.Username {
+		return nil, errors.New("permission denied: must be run with root")
+	}
 	dmi := dmidecode.New()
 	if err := dmi.Run(); nil != err {
-		return nil, errors.New("Fail to run dmidecode")
+		return nil, err
 	}
 	return nil, nil
 }
